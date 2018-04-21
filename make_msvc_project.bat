04000b4e94664494e3e1ed52ce693845528def4c
10km <10km0811@sohu.com>
2018-04-21 13:22:00 +0800

    删除 project.vs2015 增加 make_msvc_project.bat

---
+++ make_msvc_project.bat
@@ -0,0 +1,23 @@
+echo off 
+echo make jpegwrapper VS2015 project
+if not defined VS140COMNTOOLS (
+	echo vs2015 NOT FOUND.
+	exit -1
+)
+echo vs2015 found.
+where cmake
+if errorlevel 1 (
+	echo cmake NOT FOUND.
+	exit -1
+)
+echo cmake found
+rem 需要先编译feature_se
+if exist project.vs2015 rmdir project.vs2015 /s/q
+mkdir project.vs2015
+cd project.vs2015
+if not defined VisualStudioVersion (
+	echo make MSVC environment ...
+	call "%VS140COMNTOOLS%..\..\vc/vcvarsall" x86_amd64
+)
+echo creating x86_64 Project for Visual Studio 2015 ...
+cmake -G "Visual Studio 14 2015 Win64" -DCMAKE_INSTALL_PREFIX=..\release\jpegwrapper_x86_64 .. 
