6631faedaeccceb8aec8a4377b40180e07a5a9ea
10km <10km0811@sohu.com>
2018-04-21 23:45:24 +0800

    增加编译脚本

---
+++ build_jpeg_turbo_gcc.bat
@@ -0,0 +1,72 @@
+echo off 
+echo build jpeg-turbo by MinGW
+where gcc
+if errorlevel 1 (
+	echo MinGW/gcc NOT FOUND.
+	exit -1
+)
+echo MinGW/gcc found.
+where cmake
+if errorlevel 1 (
+	echo cmake NOT FOUND.
+	exit -1
+)
+echo cmake found.
+where nasm
+if errorlevel 1 (
+	echo nasm NOT FOUND.
+	exit -1
+)
+echo nasm found.
+set sh_folder=%~sdp0
+
+pushd %sh_folder%\libjpeg-turbo-1.4.2
+gcc --version |findstr "sjlj seh"
+rem 判断是否能编译64位程序
+if errorlevel 1 (
+	echo unsupported x86_64 build
+	)else call:gcc_x86_64
+	
+gcc --version |findstr "sjlj dwarf"
+rem 判断是否能编译32位程序
+if errorlevel 1 (
+	echo unsupported x86 build	
+	)else call:gcc_x86
+
+goto :end
+:gcc_x86
+echo build x86 use MinGW 
+if exist build_gcc_x86 rmdir build_gcc_x86 /s/q
+mkdir build_gcc_x86
+pushd build_gcc_x86
+rem gcc SJLJ or DWARF distribution required
+
+cmake -G "MinGW Makefiles" ^
+	-DCMAKE_BUILD_TYPE=RELEASE ^
+	-DCMAKE_C_FLAGS=-m32 ^
+	-DCMAKE_INSTALL_PREFIX=%sh_folder%/release/libjpeg-turbo-windows-gcc-x86 ^
+	..
+make install -j8
+popd
+rmdir build_gcc_x86 /s/q
+goto:eof
+
+:gcc_x86_64
+echo build x86_64 use MinGW 
+if exist build_gcc_x86_64 rmdir build_gcc_x86_64 /s/q
+mkdir build_gcc_x86_64
+pushd build_gcc_x86_64
+rem gcc SJLJ or SEH distribution required
+
+cmake -G "MinGW Makefiles" ^
+	-DCMAKE_BUILD_TYPE=RELEASE ^
+	-DCMAKE_C_FLAGS=-m64 ^
+	-DCMAKE_INSTALL_PREFIX=%sh_folder%/release/libjpeg-turbo-windows-gcc-x86_64 ^
+	..
+make install -j8
+popd
+rmdir build_gcc_x86_64 /s/q
+goto:eof
+
+:end
+popd
---
+++ build_openjpeg_msvc.bat
@@ -0,0 +1,67 @@
+echo off 
+echo build jpegwrapper by VS2015
+if not defined VS140COMNTOOLS (
+	echo vs2015 NOT FOUND.
+	exit -1
+)
+echo vs2015 found.
+where cmake
+if errorlevel 1 (
+	echo cmake NOT FOUND.
+	exit -1
+)
+echo cmake found.
+
+set sh_folder=%~sdp0
+
+pushd %sh_folder%\openjpeg-version.2.1
+
+:msvc_x86
+echo build x86 
+if exist build rmdir build /s/q
+mkdir build
+cd build
+echo make MSVC x86 environment ...
+call "%VS140COMNTOOLS%..\..\vc/vcvarsall" x86
+cmake -G "NMake Makefiles" ^
+	-DCMAKE_BUILD_TYPE=RELEASE ^
+	-DCMAKE_INSTALL_PREFIX=%sh_folder%/release/openjpeg-windows-vc-x86-mt ^
+	-DBUILD_SHARED_LIBS=OFF ^
+	-DCMAKE_USER_MAKE_RULES_OVERRIDE=%sh_folder%\cmake\compiler_flag_overrides.cmake ..
+nmake install
+del * /s/q
+cmake -G "NMake Makefiles" ^
+	-DCMAKE_BUILD_TYPE=RELEASE ^
+	-DCMAKE_INSTALL_PREFIX=%sh_folder%/release/openjpeg-windows-vc-x86 ^
+	-DBUILD_SHARED_LIBS=OFF ^
+	..
+nmake clean install
+cd ..
+
+:msvc_x86_64
+echo build x86_64 
+if exist build rmdir build /s/q
+mkdir build
+cd build
+echo make MSVC amd64 environment ...
+call "%VS140COMNTOOLS%..\..\vc/vcvarsall" x86_amd64
+
+cmake -G "NMake Makefiles" ^
+	-DCMAKE_BUILD_TYPE=RELEASE ^
+	-DCMAKE_INSTALL_PREFIX=%sh_folder%/release/openjpeg-windows-vc-x86_64-mt ^
+	-DBUILD_SHARED_LIBS=OFF ^
+	-DCMAKE_USER_MAKE_RULES_OVERRIDE=%sh_folder%\cmake\compiler_flag_overrides.cmake ..
+nmake install
+del * /s/q
+cmake -G "NMake Makefiles" ^
+	-DCMAKE_BUILD_TYPE=RELEASE ^
+	-DCMAKE_INSTALL_PREFIX=%sh_folder%/release/openjpeg-windows-vc-x86_64 ^
+	-DBUILD_SHARED_LIBS=OFF ^
+	..
+nmake clean install
+cd ..
+rmdir build/s/q
+
+:end
+popd
+
