05d3eb0f7c280e2efa706c53d6310e95e9cdfd5a
10km <10km0811@sohu.com>
2018-04-22 23:38:47 +0800

    commit

---
+++ build.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+echo build jpegwrapper 
+GXX_PATH=
+if [ `/usr/bin/g++ -dumpversion` != "5.2.0" ]
+then
+	if [ `/usr/local/bin/g++ -dumpversion` = "5.2.0" ]
+	then
+		GXX_PATH="-DCMAKE_CXX_COMPILER:FILEPATH=/usr/local/bin/g++ -DCMAKE_C_COMPILER:FILEPATH=/usr/local/bin/gcc -DCMAKE_BUILD_TYPE:STRING=RELEASE"
+	else
+		echo "g++ compiler required version 5.2.0"
+		exit -1
+	fi
+fi
+if [ -d build ]
+then 
+	rm -fr build/*
+else 
+	mkdir build
+fi
+pushd build
+
+cmake -G "Unix Makefiles" $GXX_PATH -DCMAKE_BUILD_TYPE=RELEASE -DCMAKE_INSTALL_PREFIX=../release/jpegwrapper_linux_x86_64 ..
+make install
+popd
+
+#rm -fr build
--- build_jpeg_turbo.sh
+++ build_jpeg_turbo.sh
@@ -1,78 +1,86 @@
-echo off 
+#!/bin/bash
 echo build jpeg-turbo by MinGW
-where gcc
-if errorlevel 1 (
-	echo MinGW/gcc NOT FOUND.
-	exit -1
-)
-echo MinGW/gcc found.
-where cmake
-if errorlevel 1 (
+which cmake
+if [ ! $? -eq 0 ]
+then
 	echo cmake NOT FOUND.
 	echo download from http://cmake.org/ ,extract to disk 
 	echo add installation path to environment variable PATH
-	pause
 	exit -1
-)
+fi
 echo cmake found.
-where nasm
-if errorlevel 1 (
+which nasm
+if [ ! $? -eq 0 ]
+then
 	echo nasm NOT FOUND.
 	echo download from https://www.nasm.us/ ,extract to disk 
 	echo add installation path to environment variable PATH, version above 2.13.03 required.
 	pause
 	exit -1
-)
+fi
 echo nasm found.
-set sh_folder=%~sdp0
-
-pushd %sh_folder%\libjpeg-turbo-1.4.2
-rem 判断是否能编译64位程序
-gcc --version |findstr "sjlj seh"
-if errorlevel 1 (
-	echo unsupported x86_64 build
-	)else call:gcc_x86_64
+sh_folder=$(dirname $(readlink -f $0))
+pushd $sh_folder/libjpeg-turbo-1.5.90
+# 判断是否能编译64位程序
+#gcc --version |findstr "sjlj seh"
+#if errorlevel 1 (
+#	echo unsupported x86_64 build
+#	)else call:gcc_x86_64
 	
-rem 判断是否能编译32位程序
-gcc --version |findstr "sjlj dwarf"
-if errorlevel 1 (
-	echo unsupported x86 build	
-	)else call:gcc_x86
+# 判断是否能编译32位程序
+#gcc --version |findstr "sjlj dwarf"
+#if errorlevel 1 (
+#	echo unsupported x86 build	
+#	)else call:gcc_x86
 
-goto :end
-:gcc_x86
-echo build x86 use MinGW 
-if exist build_gcc_x86 rmdir build_gcc_x86 /s/q
-mkdir build_gcc_x86
+#goto :end
+build_gcc_x86(){
+echo "build x86 use gcc"
+if [ -d build_gcc_x86 ]
+then 
+	rm -fr build_gcc_x86/*
+else 
+	mkdir build_gcc_x86
+fi
 pushd build_gcc_x86
-rem gcc SJLJ or DWARF distribution required
+#echo "gcc SJLJ or DWARF distribution required"
 
-cmake -G "MinGW Makefiles" ^
-	-DCMAKE_BUILD_TYPE=RELEASE ^
-	-DCMAKE_C_FLAGS=-m32 ^
-	-DCMAKE_INSTALL_PREFIX=%sh_folder%/release/libjpeg-turbo-windows-gcc-x86 ^
+cmake -G "Unix Makefiles" \
+	-DCMAKE_BUILD_TYPE=RELEASE \
+	-DCMAKE_C_FLAGS=-m32 \
+	-DWITH_SIMD=OFF \
+	-DCMAKE_INSTALL_PREFIX=$sh_folder/release/libjpeg-turbo-linux-x86 \
 	..
 make install -j8
 popd
-rmdir build_gcc_x86 /s/q
-goto:eof
+rm -fr build_gcc_x86
+}
 
-:gcc_x86_64
-echo build x86_64 use MinGW 
-if exist build_gcc_x86_64 rmdir build_gcc_x86_64 /s/q
-mkdir build_gcc_x86_64
+build_gcc_x86_64(){
+#echo "build x86_64 use gcc"
+if [ -d build_gcc_x86_64 ]
+then 
+	rm -fr build_gcc_x86_64/*
+else 
+	mkdir build_gcc_x86_64
+fi
+
 pushd build_gcc_x86_64
-rem gcc SJLJ or SEH distribution required
+#rem "gcc SJLJ or SEH distribution required"
 
-cmake -G "MinGW Makefiles" ^
-	-DCMAKE_BUILD_TYPE=RELEASE ^
-	-DCMAKE_C_FLAGS=-m64 ^
-	-DCMAKE_INSTALL_PREFIX=%sh_folder%/release/libjpeg-turbo-windows-gcc-x86_64 ^
+cmake -G "Unix Makefiles" \
+	-DCMAKE_BUILD_TYPE=RELEASE \
+	-DCMAKE_C_FLAGS=-m64 \
+	-DWITH_SIMD=OFF \
+	-DCMAKE_INSTALL_PREFIX=$sh_folder/release/libjpeg-turbo-linux-x86_64 \
 	..
 make install -j8
 popd
-rmdir build_gcc_x86_64 /s/q
-goto:eof
+#rm -fr build_gcc_x86_64
+}
 
-:end
+#build_gcc_x86
+build_gcc_x86_64
+
+#:end
 popd
