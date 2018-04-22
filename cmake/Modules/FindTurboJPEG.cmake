6e39fe41b18c47db5cb470514159a776b1792394
10km <10km0811@sohu.com>
2018-04-23 00:49:26 +0800

    commit

--- cmake/Modules/FindTurboJPEG.cmake
+++ cmake/Modules/FindTurboJPEG.cmake
@@ -1,5 +1,5 @@
 #.rst:
-# FindTurboJPEG
+# FindTURBOJPEG
 # --------
 #
 # Find Turbo JPEG
@@ -8,43 +8,35 @@
 #
 # ::
 #
-#   TurboJPEG_INCLUDE_DIR, where to find jpeglib.h, etc.
-#   TurboJPEG_LIBRARIES, the libraries needed to use Turbo JPEG.
-#   TurboJPEG_FOUND, If false, do not try to use Turbo JPEG.
-#   TurboJPEG_LIBRARY_STATIC Turbo JPEG static library
-#
-# also defined, but not for general use are
-#
-# ::
-#
-#   TurboJPEG_LIBRARY, where to find the Turbo JPEG library.
+#   TURBOJPEG_INCLUDE_DIR, where to find turbojpeg.h, etc.
+#   TURBOJPEG_FOUND, If false, do not try to use Turbo JPEG.
+#   TURBOJPEG_LIBRARY, where to find the Turbo JPEG library.
+#   TURBOJPEG_LIBRARY_STATIC where to find the Turbo JPEG static library
 
 
-find_path(TurboJPEG_INCLUDE_DIR turbojpeg.h)
+find_path(TURBOJPEG_INCLUDE_DIR turbojpeg.h)
 
-set(TurboJPEG_NAMES ${TurboJPEG_NAMES} turbojpeg libturbojpeg)
-find_library(TurboJPEG_LIBRARY NAMES ${TurboJPEG_NAMES} )
+set(TURBOJPEG_NAMES ${TURBOJPEG_NAMES} turbojpeg libturbojpeg)
+find_library(TURBOJPEG_LIBRARY NAMES ${TURBOJPEG_NAMES} )
 
 # handle the QUIETLY and REQUIRED arguments and set TURBOJPEG_FOUND to TRUE if
 # all listed variables are TRUE
 include(FindPackageHandleStandardArgs)
-FIND_PACKAGE_HANDLE_STANDARD_ARGS(TurboJPEG DEFAULT_MSG TurboJPEG_LIBRARY TurboJPEG_INCLUDE_DIR)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(TURBOJPEG DEFAULT_MSG TURBOJPEG_LIBRARY TURBOJPEG_INCLUDE_DIR)
 
 if(TURBOJPEG_FOUND)
-  set(TurboJPEG_LIBRARIES ${TurboJPEG_LIBRARY})
-
 	if(MSVC)
 		set(_stati_library_name ${CMAKE_STATIC_LIBRARY_PREFIX}turbojpeg-static${CMAKE_STATIC_LIBRARY_SUFFIX})
 	else()
 		set(_stati_library_name ${CMAKE_STATIC_LIBRARY_PREFIX}turbojpeg${CMAKE_STATIC_LIBRARY_SUFFIX})
 	endif()
-	find_library(TurboJPEG_LIBRARY_STATIC ${_stati_library_name})
-	if(TurboJPEG_LIBRARY_STATIC)
-		message(STATUS "TurboJPEG_LIBRARY_STATIC=${TurboJPEG_LIBRARY_STATIC}")
+	find_library(TURBOJPEG_LIBRARY_STATIC ${_stati_library_name})
+	if(TURBOJPEG_LIBRARY_STATIC)
+		message(STATUS "TURBOJPEG_LIBRARY_STATIC=${TURBOJPEG_LIBRARY_STATIC}")
 	else()
 		message(STATUS "NOT FOUND jpeg-turbo static library")
 	endif()
 	unset(_stati_library_name)
 endif()
 
-mark_as_advanced(TurboJPEG_LIBRARY TurboJPEG_INCLUDE_DIR )
+mark_as_advanced(TURBOJPEG_LIBRARY TURBOJPEG_INCLUDE_DIR )
