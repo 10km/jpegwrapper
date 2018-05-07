64c2b51eb352880bb3680d9ad970f29279285270
10km <10km0811@sohu.com>
2018-05-07 21:59:06 +0800

    改变生成Makefile方式，以便eclipse能正确导入工程

--- test/make_mingw_makefile.bat
+++ test/make_mingw_makefile.bat
@@ -13,7 +13,7 @@ if errorlevel 1 (
 )
 echo cmake found
 set sh_folder=%~dp0
-pushd %sh_folder%
+pushd %sh_folder%\..
 rem 判断是否能编译64位程序
 gcc --version |findstr "sjlj seh"
 if errorlevel 1 (
@@ -21,16 +21,18 @@ if errorlevel 1 (
 	paush 
 	exit /B -1
 	)
-if exist build.gcc rmdir build.gcc /s/q
-mkdir build.gcc
-pushd build.gcc
+if exist test.mingw rmdir test.mingw /s/q
+mkdir test.mingw
+pushd test.mingw
 set sh_folder=%sh_folder:\=/%
 echo creating x86_64 Project for MinGW ...
-cmake -G "MinGW Makefiles" ^
+cmake -G "Eclipse CDT4 - MinGW Makefiles" ^
 	-DCMAKE_MODULE_PATH=%sh_folder%../cmake/Modules ^
 	-DCMAKE_BUILD_TYPE=DEBUG ^
 	-DCMAKE_PREFIX_PATH=%sh_folder%../release/jpegwrapper-windows-gcc-x86_64;^%sh_folder%../dependencies/libjpeg-turbo-windows-gcc-x86_64;%sh_folder%../dependencies/openjpeg-windows-gcc-x86_64 ^
-	..
+	%sh_folder%
 	
 popd
 popd
+
+goto :eof
--- test/make_unix_makefile.sh
+++ test/make_unix_makefile.sh
@@ -4,24 +4,31 @@ if [ `/usr/bin/g++ -dumpversion` != "5.2.0" ]
 then
 	if [ `/usr/local/bin/g++ -dumpversion` = "5.2.0" ]
 	then
-		GXX_PATH="-DCMAKE_CXX_COMPILER:FILEPATH=/usr/local/bin/g++ -DCMAKE_C_COMPILER:FILEPATH=/usr/local/bin/gcc -DCMAKE_BUILD_TYPE:STRING=RELEASE"
+		GXX_PATH="-DCMAKE_CXX_COMPILER:FILEPATH=/usr/local/bin/g++ -DCMAKE_C_COMPILER:FILEPATH=/usr/local/bin/gcc"
 	else
 		echo "g++ compiler required version 5.2.0"
 		exit -1
 	fi
 fi
-sh_folder=$(dirname $(readlink -f $0))
-cd $sh_folder
-if [ -d build.gcc ]
-then 
-	rm -fr build.gcc/*
-else 
-	mkdir build.gcc
-fi
 
-cd build.gcc
-cmake $GXX_PATH -G "Unix Makefiles" .. \
+sh_folder=$(dirname $(readlink -f $0))
+folder_name=$(basename $sh_folder) 
+# 定义编译的版本类型(DEBUG|RELEASE)
+build_type=DEBUG
+typeset -u arg1=$1
+[ "$arg1" = "DEBUG" ] && build_type=$arg1
+echo build_type=$build_type
+
+pushd $sh_folder/..
+
+[ -d $folder_name.gcc ] && rm -fr $folder_name.gcc
+mkdir $folder_name.gcc
+
+pushd $folder_name.gcc
+cmake "$sh_folder" $GXX_PATH -G "Eclipse CDT4 - Unix Makefiles" -DCMAKE_BUILD_TYPE=$build_type \
 	-DCMAKE_MODULE_PATH=$sh_folder/../cmake/Modules \
 	-DCMAKE_PREFIX_PATH="$sh_folder/../release/jpegwrapper-linux-x86_64;$sh_folder/../dependencies/libjpeg-turbo-linux-x86_64;$sh_folder/../dependencies/openjpeg-linux-x86_64" 
+popd
 
+popd
 
